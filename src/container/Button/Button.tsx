85bdb0e6625b27abf4a3f66019604db5975f7091
Pronay Guha <pronay.guha@thermofisher.com>
2025-04-20 03:18:21 +0530

    Overall layout and button part styling done

--- src/container/Button/Button.tsx
+++ src/container/Button/Button.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./styles.module.css";
 import Button from "../../components/Button/Button";
 
-const numericButtons = [...Array(10).keys()];
-const operatorButtons = ["+", "-", "*", "/", "CE", "="];
+const numericButtons = [7, 8, 9, 6, 5, 4, 3, 2, 1, 0];
+const sideOperatorButtons = ["+", "="];
+
+const topOperatorButtons = ["CE", "/", "*", "-"];
 
 const ButtonContainer = () => {
   const handleClick = (event: MouseEvent, character: string) => {
@@ -16,24 +18,58 @@ const ButtonContainer = () => {
   };
 
   const getNumericButtons = () => {
-    return numericButtons.map((numericButton, index) => (
-      <Button
-        key={index}
-        character={numericButton.toString()}
-        onClickHandler={handleClick}
-      />
-    ));
+    return (
+      <div className={styles.numeric_button_container}>
+        {numericButtons.map((numericButton, index) => (
+          <Button
+            key={index}
+            character={numericButton.toString()}
+            onClickHandler={handleClick}
+          />
+        ))}
+      </div>
+    );
   };
 
-  const getOperatorButtons = () => {
-    return operatorButtons.map((opBtn, index) => (
-      <Button key={index} character={opBtn} onClickHandler={handleClick} />
-    ));
+  const getTopOperatorButtons = () => {
+    return (
+      <div className={styles.top_operator_button_container}>
+        {topOperatorButtons.map((opBtn, index) => (
+          <Button key={index} character={opBtn} onClickHandler={handleClick} />
+        ))}
+      </div>
+    );
+  };
+
+  const getSideOperatorButtons = () => {
+    return (
+      <div className={styles.side_operator_button_container}>
+        {sideOperatorButtons.map((opBtn, index) => (
+          <Button key={index} character={opBtn} onClickHandler={handleClick} />
+        ))}
+      </div>
+    );
+  };
+
+  const getSpecialButtons = () => {
+    return (
+      <div className={styles.special_button_container}>
+        {[".", "+/-"].map((specialCharacter, index) => (
+          <Button
+            key={index}
+            character={specialCharacter}
+            onClickHandler={handleClick}
+          />
+        ))}
+      </div>
+    );
   };
   return (
     <div className={styles.container}>
-      {getOperatorButtons()}
+      {getTopOperatorButtons()}
+      {getSideOperatorButtons()}
       {getNumericButtons()}
+      {getSpecialButtons()}
     </div>
   );
 };
